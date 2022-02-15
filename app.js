d50329f09f9dff3691f4ee9788ed4a264d0b7690
Chris Pietrangeli <chris.pietrangeli11@gmail.com>
2022-02-14 21:21:06 -0700

    webpage

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2);
 console.log(profileDataArgs);
+const [name, github] = profileDataArgs;
+console.log(name, github);
 
-const printProfileData = profileDataArr => {
-    //This...
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-    console.log('==========');
+const pageHTML = generatePage(name, github);
 
-    //Is the same as this
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-printProfileData(profileDataArgs);
+
+
+fs.writeFile('index.html', pageHTML, err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
