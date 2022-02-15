3f302157d8cd13ec6f42b1f443114b3ee9788ba4
Chris Pietrangeli <chris.pietrangeli11@gmail.com>
2022-02-14 21:58:34 -0700

    adding inquirer

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
-console.log(profileDataArgs);
-const [name, github] = profileDataArgs;
-console.log(name, github);
+const inquirer  = require('inquirer');
 
-const pageHTML = generatePage(name, github);
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+//const fs = require('fs');
+//const generatePage = require('./src/page-template.js');
+
+//const pageHTML = generatePage(name, github);
 
 
 
-fs.writeFile('index.html', pageHTML, err => {
-    if (err) throw err;
+//fs.writeFile('index.html', pageHTML, err => {
+    //if (err) throw err;
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+    //console.log('Portfolio complete! Check out index.html to see the output!');
+//});
